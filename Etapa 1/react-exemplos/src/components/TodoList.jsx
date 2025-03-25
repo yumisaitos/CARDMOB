c369bf5a3deafe9ae7a909480b82ae0e873afd42
yumisaitos <yumisaito15@icloud.com>
2025-03-25 10:04:29 -0300

    Cancel editing

--- Etapa 1/react-exemplos/src/components/TodoList.jsx
+++ Etapa 1/react-exemplos/src/components/TodoList.jsx
@@ -37,6 +37,12 @@ const TodoList = ({name}) => {
         setTodos(todos.filter((todo) => todo.id !== id));
     }
 
+    //cancel editing
+    const cancelEditing = () => {
+        setEditingId(null);
+        setEditingText("");
+    }
+
     return (
         <div style={{textAlign: "center", marginTop: "50px"}}>
             <h2>A fazer: {name}</h2>
@@ -59,6 +65,7 @@ const TodoList = ({name}) => {
                                     onChange={(event) => setEditingText(event.target.value)}
                                 />
                                 <button onClick={saveEdit}>Salvar</button>
+                                <a href="#" onClick={cancelEditing}>Cancelar</a>
                             </>
                         ) : (
                             <>
