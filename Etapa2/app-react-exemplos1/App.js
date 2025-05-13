154e983bc941f8d4d4a57e3fc25c816659cf54a6
yumi saito <yumisaito15@icloud.com>
2025-05-13 10:31:59 -0300

    CRUD simples: addItem

--- Etapa2/app-react-exemplos1/App.js
+++ Etapa2/app-react-exemplos1/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
 
 export default function App() {
   const [counter, setCounter] = useState(0);
+  /// CRUD em memória
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState('');
+  const [editItemId, setEditItemId] = useState(null);
+  const [editItemText, setEditItemText] = useState('');
 
   const incrementCounter = () => {
     setCounter(counter + 1);
@@ -13,9 +18,32 @@ export default function App() {
     setCounter(counter - 1);
   };
 
-  /// CRUD em memória
+  const addItem = () => {
+    if (text.trim() === '') {
+      return;
+    }
+    const newItem = {
+      id: Math.random().toString(),
+      text: text.trim()
+    }
+    setItems([...items, newItem]);
+    setText('');
+    console.log(items);
+  }
+
+  
   return (
     <View style={styles.container}>
+      <TextInput 
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+        placeholder='Enter text item'
+      />
+      <Button 
+        title='Add Item'
+        onPress={addItem}
+      />
       <Text style={styles.text}>Olá App React Native - Atualiza!</Text>
       <Image 
         source={{uri: "https://picsum.photos/200"}}
