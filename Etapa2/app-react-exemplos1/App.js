33d9a94dbed07a2513d15b3635c32fe92687b83c
yumi saito <yumisaito15@icloud.com>
2025-05-20 07:20:45 -0300

    View basic examples

--- Etapa2/app-react-exemplos1/App.js
+++ Etapa2/app-react-exemplos1/App.js
@@ -9,53 +9,36 @@ export default function App() {
   return (
     <View style={styles.container}>
       <List />
+      <View style={styles.redbox}></View>
+      <View style={styles.bluebox}></View>
+      <View style={styles.blackbox}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 20,
-    marginTop: 60,
-  },
-  text: {
-    fontSize: 24,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-  list: {
-    marginTop: 20,
-  },
-  item: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'space-between',
-    marginBottom: 10,
-    padding: 10,
-    backgroundColor: '#f0f0f0',
-    borderRadius: 5,
+    alignItems: 'center',
+    backgroundColor: 'gray',
+    height: 600,
+    marginTop: 150,
   },
-  itemText: {
-    flex: 1,
-    marginRight: 10,
+  redbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+    borderRadius: 50,
   },
-  buttons: {
-    flexDirection: 'row',
+  bluebox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
   },
-  editInput: {
-    flex: 1,
-    marginRight: 10,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingHorizontal: 10,
+  blackbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black',
   }
 });
