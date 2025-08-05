16f8b55923973d3f0e68e94daa77085b244b6ed7
yumisaitos <yumisaito15@icloud.com>
2025-08-05 09:02:38 -0300

    Inclui tela HomeScreen

---
+++ Etapa3/loja-react-app/src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function HomeScreen() {
+
+    return (
+        <View>
+            <text>Home Screen</text>
+        </View>
+    );
+}
+
+export default HomeScreen;
