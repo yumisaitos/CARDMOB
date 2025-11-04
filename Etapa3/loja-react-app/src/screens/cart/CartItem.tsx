d1fb92a5fbef7bff01147713998d60d95699ad46
yumisaitos <yumisaito15@icloud.com>
2025-11-04 07:42:30 -0300

    carrinho de compras mínimo

--- Etapa3/loja-react-app/src/screens/cart/CartItem.tsx
+++ Etapa3/loja-react-app/src/screens/cart/CartItem.tsx
@@ -1,14 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
-import { useShop } from "../../contexts/ShopContext";
 
 const CartItem = ({ item }: any) => {
     // @todo implementar o context para persistir dados do carrinho.
-    const { addToCart, removeFromCart } = useShop();
 
-    const handleRemove = (item: any) => {
-        removeFromCart(item.id);
+    const handleRemove = () => {
         console.log('exclui produto');
     }
 
@@ -19,17 +16,17 @@ const CartItem = ({ item }: any) => {
                 <Text style={styles.name}>{item.name}</Text>
                 <View style={styles.quantity}>
                     <Text style={styles.price}>R$ {(item.price * item.quantity).toFixed(2)}</Text>
-                    <TouchableOpacity onPress={() => addToCart(item, -1)} style={styles.button}>
+                    <TouchableOpacity onPress={() => handleRemove()} style={styles.button}>
                         <Text style={styles.buttonText}>-</Text>
                     </TouchableOpacity>
                     <Text style={styles.quantityValue}>{item.quantity}</Text>
 
-                    <TouchableOpacity onPress={() => addToCart(item)} style={styles.button}>
+                    <TouchableOpacity onPress={() => handleRemove()} style={styles.button}>
                         <Text style={styles.buttonText}>+</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity onPress={() => handleRemove(item)} style={styles.removeButton}>
-                        <Text style={styles.removeButtonText}>Remover</Text>
+                    <TouchableOpacity onPress={() => handleRemove()} style={styles.button}>
+                        <Text style={styles.buttonText}>Remover</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -48,46 +45,16 @@ const styles = StyleSheet.create({
         borderColor: '#ddd',
     },
     image: {
-        width: 100,
+        width: '50%',
         height: 100,
-        borderRadius: 10,
+        borderRadius: 8,
         borderColor: '#ddd',
-        marginRight: 10
     },
-    name: {
-        fontSize: 16,
-    },
-    quantity: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginVertical: 5,
-    },
-    price: {
-        fontSize: 14,
-        color: '#007BFF',
-    },
-    button: {
-        backgroundColor: '#007BFF',
-        borderRadius: 5,
-        padding: 5,
-        marginHorizontal: 5,
-    },
-    buttonText: {
-        fontSize: 18,
-        color: '#fff',
-    },
-    quantityValue: {
-        marginHorizontal: 10,
-        fontSize: 16,
-    },
-    removeButton: {
-        marginLeft: 10,
-        backgroundColor: '#FF0000',
-        padding: 10,
-        borderRadius: 5,
-    },
-    removeButtonText: {
-        color: '#fff',
-        textAlign: 'center'
-    },
+    name: {},
+    quantity: {},
+    price: {},
+    button: {},
+    buttonText: {},
+    quantityValue: {},
+
 });
--- Etapa3/loja-react-app/src/screens/cart/CartScreen.tsx
+++ Etapa3/loja-react-app/src/screens/cart/CartScreen.tsx
@@ -1,11 +1,32 @@
 import React, { useContext } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+import CartItem from './CartItem';
 
 const CartScreen = ({ navigation }: any) => {
 
+    const renderItem = ({item} : any) => (
+        <CartItem item={item} />
+    );
+
+    const cartTemp = [
+        { 
+            id: 1,
+            quantity: 2,
+            price: 8.5,
+            name: "Brigadeiro promoção especial",
+            image: "http://10.81.205.50:5000/uploads/brigadeiro-matcha-white.png",
+        }
+    ];
+
     return (
         <View> 
-            <Text> Carrinho de compras</Text>
+            <Text>Carrinho de compras</Text>
+            <FlatList 
+                data={cartTemp}
+                renderItem={renderItem}
+                keyExtractor={(item: any) => item.id.toString()}
+            />
         </View>
     );
 };
